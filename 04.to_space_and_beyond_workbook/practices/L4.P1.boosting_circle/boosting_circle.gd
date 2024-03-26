7f33cc1cdc16b8c59bc1e63b3f5a5ab47cc890aa
ylirin <xili1@s.sfusd.edu>
2024-03-26 14:23:24 -0700

    L4. The Boosting Ship

--- 04.to_space_and_beyond_workbook/practices/L4.P1.boosting_circle/boosting_circle.gd
+++ 04.to_space_and_beyond_workbook/practices/L4.P1.boosting_circle/boosting_circle.gd
@@ -1,7 +1,6 @@
 extends Sprite2D
 
 var boost_speed := 1200.0
-var normal_speed := 600.0
 
 var max_speed := normal_speed
 var velocity := Vector2(0, 0)
@@ -17,13 +16,14 @@ func _process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("boost"):
 		# Replace the pass keyword with the code to change the max_speed, get the timer node, and start it.
-		pass
+		max_speed = boost_speed
+		get_node("Timer").start()
 
 	velocity = direction * max_speed
 	position += velocity * delta
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
 
-
+var normal_speed := 600.0
 func _on_timer_timeout() -> void:
-	pass
+	max_speed = normal_speed
