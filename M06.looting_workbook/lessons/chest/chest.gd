043e8a7df7067aacabc275866935f8accc2f75c7
ylirin <xili1@s.sfusd.edu>
2024-04-29 14:27:18 -0700

    L6. Spawn random loot

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -1,4 +1,6 @@
 extends Area2D
+@export var possible_items: Array[PackedScene] = []
+
 @onready var canvas_group: CanvasGroup = $CanvasGroup
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -32,3 +34,19 @@ func open() -> void:
 	animation_player.play("open")
 	
 	input_pickable = false
+	
+	if possible_items.is_empty():
+		return
+		
+	for current_index in range(randi_range(1,3)):
+		_spawn_random_item()
+	
+func _spawn_random_item() -> void:
+	var loot_item: Area2D = possible_items.pick_random().instantiate()
+	add_child(loot_item)
+	
+	var random_angle := randf_range(0.0, 2.0 * PI)
+	var random_direction := Vector2(1.0, 0.0).rotated(random_angle)
+	var random_distance:= randf_range(60.0, 120.0)
+	loot_item.position = random_direction * random_distance
+	
