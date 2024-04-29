70d6ae35b48d9193ae9e62b12c56bcc839b6d659
ylirin <xili1@s.sfusd.edu>
2024-04-29 13:52:28 -0700

    L4. and L5.

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -1,10 +1,11 @@
 extends Area2D
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
+	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
 
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
@@ -12,9 +13,22 @@ func set_outline_thickness(new_thickness: float) -> void:
 func _on_mouse_entered() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 3.0, 6.0, 0.08)
-	canvas_group.material.set_shader_parameter("line_thickness", 6.0)
 	
 func _on_mouse_exited() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
-	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
+	
+func _input_event(viewport: Node, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+		)
+	
+	if event_is_mouse_click:
+		open()
+
+func open() -> void:
+	animation_player.play("open")
+	
+	input_pickable = false
