287a1dccfa42afc11d3fd7acb9d7c5b8085370f5
ylirin <xili1@s.sfusd.edu>
2024-04-29 14:42:15 -0700

    L5. UPDATE

--- M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
+++ M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
@@ -6,8 +6,15 @@ extends Area2D
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 	# Complete this boolean expression to check if the input event is a left-mouse click.
 	var event_is_mouse_click: bool = (
-		false
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
 	)
-
+	
 	if event_is_mouse_click:
-		pass
+		open()
+
+func open() -> void:
+	animation_player.play("open")
+	
+	input_pickable = false
--- M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
+++ M06.looting_workbook/practices/L5.P2.pop_the_balloon/balloon_pop.gd
@@ -4,8 +4,9 @@ extends Area2D
 func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 	var event_is_mouse_release: bool = (
 		event is InputEventMouseButton
-		and event.button_index == MOUSE_BUTTON_LEFT
+		and event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_released()
 	)
 
 	if event_is_mouse_release:
-		pass
+		queue_free()
