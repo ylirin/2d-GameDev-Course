5497fb85771b68f1d17c5e1f9bc544b79d60efae
ylirin <xili1@s.sfusd.edu>
2024-04-30 13:47:00 -0700

    L7. Animate the collectibles

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -31,14 +31,14 @@ func _input_event(viewport: Node, event: InputEvent, shape_index: int):
 		open()
 
 func open() -> void:
-	animation_player.play("open")
-	
 	input_pickable = false
+	
+	get_node("AnimationPlayer").play("open", 0.1)
 	
 	if possible_items.is_empty():
 		return
 		
-	for current_index in range(randi_range(1,3)):
+	for current_index in range(randi_range(1,7)):
 		_spawn_random_item()
 	
 func _spawn_random_item() -> void:
@@ -48,5 +48,21 @@ func _spawn_random_item() -> void:
 	var random_angle := randf_range(0.0, 2.0 * PI)
 	var random_direction := Vector2(1.0, 0.0).rotated(random_angle)
 	var random_distance:= randf_range(60.0, 120.0)
-	loot_item.position = random_direction * random_distance
+	var land_position := random_direction * random_distance
 	
+	const FLIGHT_TIME := 0.4
+	const HALF_FLIGHT_TIME := FLIGHT_TIME / 2.0
+	
+	var tween := create_tween()
+	tween.set_parallel()
+	loot_item.scale = Vector2(0.25, 0.25)
+	tween.tween_property(loot_item, "scale", Vector2(1.0,1.0), HALF_FLIGHT_TIME)
+	tween.tween_property(loot_item, "position:x", land_position.x, FLIGHT_TIME)
+	
+	tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	var jump_height := randf_range(30.0, 80.0)
+	tween.tween_property(loot_item, "position:y", land_position.y - jump_height, HALF_FLIGHT_TIME)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(loot_item, "position:y", land_position.y, HALF_FLIGHT_TIME)
